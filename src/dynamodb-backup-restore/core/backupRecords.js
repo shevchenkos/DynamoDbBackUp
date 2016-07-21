9c0f72d6b0828a9a67dd80e21129462235d212f4
s.shevchenko <s.shevchenko@levi9.com>
2016-07-21 22:36:25 +0300

    Optimized Backup Records class

--- src/dynamodb-backup-restore/core/backupRecords.js
+++ src/dynamodb-backup-restore/core/backupRecords.js
@@ -10,11 +10,18 @@ class BackupRecords {
         this.Prefix = config.S3Prefix;
     }
 
-    backupAll(records) {
+    fromDbStream(records) {
         let allRecords = records.reduce((allRecords, action) => {
             let id = JSON.stringify(action.dynamodb.Keys);
             allRecords[id] = allRecords[id] || [];
-            allRecords[id].push(action);
+            console.log(action);
+            let change = {
+                keys: id,
+                data: JSON.stringify(action.dynamodb.NewImage),
+                event: action.eventName,
+                table: action.eventSourceARN.split('/')[1]
+            };
+            allRecords[id].push(change);
             return allRecords;
         }, {});
 
@@ -23,6 +30,10 @@ class BackupRecords {
             promises.push(this.backupRecord(allRecords[key]));
         });
         return Promise.all(promises);
+    }
+
+    fromDbInstance(records) {
+        
     }
 
     backupRecord(changes) {
@@ -38,9 +49,9 @@ class BackupRecords {
             let s3 = new AWS.S3({ region: this.Region, signatureVersion: "v4" });
 
             let id = crypto.createHash('md5')
-                .update(JSON.stringify(change.dynamodb.Keys))
+                .update(change.keys)
                 .digest('hex');
-            let table = change.eventSourceARN.split('/')[1];
+            let table = change.table;
             let key = [this.Prefix, table, id].join('/');
 
             let params = {
@@ -48,15 +59,14 @@ class BackupRecords {
                 Key: key
             };
 
-            let req = change.eventName === 'REMOVE' ? 'deleteObject' : 'putObject';
-            if (req === 'putObject') params.Body = JSON.stringify(change.dynamodb.NewImage);
+            let req = change.event === 'REMOVE' ? 'deleteObject' : 'putObject';
+            if (req === 'putObject') params.Body = change.data;
 
             s3[req](params, (err) => {
                 if (err) {
                     console.log(
                         '[error] %s | %s s3://%s/%s | %s',
-                        JSON.stringify(change.dynamodb.Keys),
-                        req, params.Bucket, params.Key, err.message
+                        change.keys, req, params.Bucket, params.Key, err.message
                     );
                     return reject(err);
                 }

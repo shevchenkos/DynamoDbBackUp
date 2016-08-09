6474593ac1cd98977228bb512f2e48fe02f8ab9c
OlegSirbu <olegsirbu2@gmail.com>
2016-08-09 15:55:47 +0300

    added tests for check travis

--- tests/backup.test.js
+++ tests/backup.test.js
@@ -3,6 +3,12 @@ var backup = require('./../lib/backup');
 
 describe('test', function () {
     it('should return true', function () {
-        expect(true).toBe(true)
+        expect(true).toBe(true);
+    });
+});
+
+describe('test', function () {
+    it('should return false', function () {
+        expect(false).toBe(false);
     });
 });
